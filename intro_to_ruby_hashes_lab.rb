37eecfad9a0bab4a3d373d1b23b84a9afbfa1840
indiana danaher <indiana.danaher@gmail.com>
2020-03-14 20:12:20 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -16,7 +16,7 @@ end
 def pioneer
   name = "key"
   hash2 = {
-    :name => "Grace Hopper",
+    name: "Grace Hopper",
   }
   return hash2
   # return a hash with a key of :name and a corresponding value of 'Grace Hopper'
